3a703c737ad92214cb7ac77abfa95bcb18eb501e
unknown <snipz722@gmail.com>
2018-08-10 19:57:04 -0400

    Created Dir

---
+++ Assets/CreateMap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMap : MonoBehaviour {
+
+	public GameObject cube;
+
+	void Start () 
+	{
+
+		for(int i=0; i < 10; i++){
+			for(int j=0; j < 10; j++){
+				Instantiate(cube, new Vector3(i,0,-j), Quaternion.identity);
+			}
+		}
+		
+	}
+	
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	void Update () 
+	{
+		GetInput();
+	}
+
+	void GetInput()
+	{
+		Vector3 pos = transform.position;
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			pos.z += 1;
+			transform.position = pos;
+			return;
+		}else if(Input.GetKeyDown(KeyCode.S))
+		{
+			pos.z -= 1;
+			transform.position = pos;
+			return;
+		}else if(Input.GetKeyDown(KeyCode.D))
+		{
+			pos.x += 1;
+			transform.position = pos;
+			return;
+		}else if(Input.GetKeyDown(KeyCode.A))
+		{
+			pos.x -= 1;
+			transform.position = pos;
+			return;
+		}
+	}
+}
